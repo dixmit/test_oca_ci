bd436df76620c3fe7e49a75179802962517aeba2
Enric Tobella <enric.tobella@dixmit.com>
2026-09-19 09:01:16 +0200

    Initial module

---
+++ module_test_for_share/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Test Module",
+    "version": "20.0.1.0.0",
+    "summary": "A test module for OCA CI",
+    "category": "Tools",
+    "author": "Your Name, Odoo Community Association (OCA)",
+    "depends": ["base"],
+    "data": [],
+    "website": "https://github.com/OCA/",
+    "installable": True,
+    "license": "AGPL-3",
+    "application": False,
+}
